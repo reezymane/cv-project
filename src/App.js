5d7bc1b98ac92af776bb0510a0012eb226910704
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-09 07:52:53 -0600

    add state to constructor

--- src/App.js
+++ src/App.js
@@ -2,17 +2,33 @@ import React, { Component } from 'react';
 import Educational from './components/Educational';
 import GeneralInfo from './components/GeneralInfo';
 import WorkHistory from './components/WorkHistory';
+import uniqid from 'uniqid';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+
+    this.state = {
+      name: {
+        text: '',
+        id: uniqid()
+      },
+      email: {
+        text: '',
+        id: uniqid()
+      },
+      phone: {
+        text: '',
+        id: uniqid()
+      }
+    };
   };
   
   render() {
     return (
       <div className="App">
+        <GeneralInfo info={this.state}/>
         <Educational />
-        <GeneralInfo />
         <WorkHistory />
       </div>
     );
