f667c483f1860b0856b604a2ad953c2569606be8
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-09 08:05:11 -0600

    add props to value attribute in form fields

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -9,13 +9,13 @@ export default class GeneralInfo extends Component {
         return (
             <form>
                 <label htmlFor='name'>Name:</label><br />
-                <input type='text' id='name' value='' onChange=''></input><br />
+                <input type='text' id='name' value={this.props.info.name.text}></input><br />
 
                 <label htmlFor='email'>Email:</label><br />
-                <input type='email' id='email' value='' onChange=''></input><br />
+                <input type='email' id='email' value={this.props.info.email.text}></input><br />
 
                 <label htmlFor='phone'>Phone:</label><br />
-                <input type='phone' id='phone' value='' onChange=''></input><br />
+                <input type='phone' id='phone' value={this.props.info.phone.text}></input><br />
 
                 <button>Submit</button>
                 <button>Edit</button>
