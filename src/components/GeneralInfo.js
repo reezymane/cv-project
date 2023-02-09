76e06967175fc3cd5bad1996455a031b8c756d79
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-09 07:46:16 -0600

    add input fields for name, email & phone

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -8,6 +8,15 @@ export default class GeneralInfo extends Component {
     render() {
         return (
             <form>
+                <label htmlFor='name'>Name:</label><br />
+                <input type='text' id='name' value='' onChange=''></input><br />
+
+                <label htmlFor='email'>Email:</label><br />
+                <input type='email' id='email' value='' onChange=''></input><br />
+
+                <label htmlFor='phone'>Phone:</label><br />
+                <input type='phone' id='phone' value='' onChange=''></input><br />
+
                 <button>Submit</button>
                 <button>Edit</button>
             </form>
