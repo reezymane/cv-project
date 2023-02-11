783d696fcf2d5060b656f81b7e9e478b1ac8a569
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-11 16:12:28 -0600

    create handleNameChange function and use as attribute

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -3,6 +3,12 @@ import React, { Component } from 'react';
 export default class GeneralInfo extends Component {
     constructor(props) {
         super(props);
+
+        this.handleNameChange = this.handleNameChange.bind(this);
+    };
+
+    handleNameChange = (event) => {
+        this.props.onNameChange(event.target.value);
     };
 
     render() {
@@ -11,7 +17,7 @@ export default class GeneralInfo extends Component {
         return (
             <form>
                 <label htmlFor='name'>Name:</label><br />
-                <input type='text' id='name' value={info.name.text}></input><br />
+                <input type='text' id='name' onChange={this.handleNameChange} value={info.name.text}></input><br />
 
                 <label htmlFor='email'>Email:</label><br />
                 <input type='email' id='email' value={info.email.text}></input><br />
