8c57fdffdbfe6770de7c3e3ffb49a5a2dfd35c57
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-11 15:55:49 -0600

    deconstruct prop

--- src/components/GeneralInfo.js
+++ src/components/GeneralInfo.js
@@ -6,16 +6,18 @@ export default class GeneralInfo extends Component {
     };
 
     render() {
+    const {info} = this.props;
+
         return (
             <form>
                 <label htmlFor='name'>Name:</label><br />
-                <input type='text' id='name' value={this.props.info.name.text}></input><br />
+                <input type='text' id='name' value={info.name.text}></input><br />
 
                 <label htmlFor='email'>Email:</label><br />
-                <input type='email' id='email' value={this.props.info.email.text}></input><br />
+                <input type='email' id='email' value={info.email.text}></input><br />
 
                 <label htmlFor='phone'>Phone:</label><br />
-                <input type='phone' id='phone' value={this.props.info.phone.text}></input><br />
+                <input type='phone' id='phone' value={info.phone.text}></input><br />
 
                 <button>Submit</button>
                 <button>Edit</button>
