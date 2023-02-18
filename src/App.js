1eb49f24eadad58b7cb9bb35e35e290d44e3dee8
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-18 16:39:57 -0600

    add form details in EducationalForm

--- src/App.js
+++ src/App.js
@@ -20,12 +20,26 @@ class App extends Component {
       phone: {
         text: '',
         id: uniqid()
+      },
+      school: {
+        text: '',
+        id: uniqid()
+      },
+      study: {
+        text: '',
+        id: uniqid()
+      },
+      dateStudy: {
+        text: '',
+        id: uniqid()
       }
     };
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.onGeneralSubmit = this.onGeneralSubmit.bind(this);
+    this.onGeneralEdit = this.onGeneralEdit.bind(this);
   };
 
   handleNameChange = (event) => {
@@ -75,52 +89,93 @@ class App extends Component {
 
     document.querySelector('.GeneralForm').style.display = 'none';
   };
+
+  onGeneralEdit = () => {
+    document.querySelector('.GeneralForm').style.display = 'block';
+  };
   
   render() {
-    const {name, email, phone} = this.state;
+    const {
+      name, 
+      email,
+      phone,
+      school,
+      study,
+      dateStudy
+    } = this.state;
 
     return (
       <div className="App">
         <div className='GeneralFormAndInfo'>
+          <p className='GeneralHeading'>General Information</p>
+
           <form className='GeneralForm' onSubmit={this.onGeneralSubmit}>
             <label htmlFor='name'>Name:</label><br />
-              <input
-               type='text'
-               id='name'
-               onChange={this.handleNameChange}
-               value={name.text}
-              /><br />
+            <input
+             type='text'
+             id='name'
+             onChange={this.handleNameChange}
+             value={name.text}
+            /><br />
 
-              <label htmlFor='email'>Email:</label><br />
-              <input
-               type='email'
-               id='email' 
-               onChange={this.handleEmailChange}
-               value={email.text}
-              /><br />
+            <label htmlFor='email'>Email:</label><br />
+            <input
+             type='email'
+             id='email' 
+             onChange={this.handleEmailChange}
+             value={email.text}
+            /><br />
 
-              <label htmlFor='phone'>Phone:</label><br />
-              <input type='phone'
-               id='phone'
-               onChange={this.handlePhoneChange}
-               value={phone.text}
-              /><br />
+            <label htmlFor='phone'>Phone:</label><br />
+            <input
+             type='phone'
+             id='phone'
+             onChange={this.handlePhoneChange}
+             value={phone.text}
+            /><br />
 
-              <button type='submit'>Submit</button>
+            <button type='submit'>Submit</button>
           </form>
 
           <GeneralInfo name={name} email={email} phone={phone}/>
 
-          <button type='button' className='EditButton'>Edit</button>
+          <button type='button' className='EditButton' onClick={this.onGeneralEdit}>Edit</button>
         </div>
 
         <div className='EducationalFormAndInfo'>
+          <p className='EducationalHeading'>Educational Experience</p>
+
           <form className='EducationalForm'>
-              <button>Submit</button>
-              <button>Edit</button>
+            <label htmlFor='school'>School Name:</label><br />
+            <input
+             type='text'
+             id='school'
+             //onChange={}
+             value={school.text}
+             /><br />
+
+            <label htmlFor='study'>Area of Study:</label><br />
+            <input
+             type='text'
+             id='study'
+             //onChange={}
+             value={study.text}
+             /><br />
+
+            <label htmlFor='dateStudy'>Date of Study:</label><br />
+            <input
+             type='text'
+             id='dateStudy'
+             //onChange={}
+             value={dateStudy.text}
+             /><br />
+
+            <button>Submit</button>
           </form>
 
           <Educational />
+
+          <button>Edit</button>
         </div>
 
         <div className='WorkHistoryFormAndInfo'>
