d642a56fb50b3a180251be1bb2020a01a04ea8b5
Deonte McCoy <deontemccoy19@gmail.com>
2023-02-18 18:10:18 -0600

    add work history info into div

--- src/components/WorkHistory.js
+++ src/components/WorkHistory.js
@@ -6,9 +6,14 @@ export default class WorkHistory extends Component {
     };
 
     render() {
+        const {companyName, positionTitle, mainTasks, dateEmployed} = this.props;
+
         return (
-            <div>
-                
+            <div className="WorkInfo">
+                <p>Company Name: {companyName.text}</p>
+                <p>Position Title: {positionTitle.text}</p>
+                <p>Main Tasks: {mainTasks.text}</p>
+                <p>Date of Employment: {dateEmployed.text}</p>
             </div>
         );
     };
